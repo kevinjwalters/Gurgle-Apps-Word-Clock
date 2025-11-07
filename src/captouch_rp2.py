0fe97f9fde440ceeb450d5a7172c92e1e51e1116
Kevin J Walters <kjw.github@gmail.com>
2025-11-07 17:28:53 +0000

    Drift compensation and offering fixed range for level.
    self.level_hi now tracks all values below the press threshold with smoothing for proper drift compensation.
    self.level_lo has some smoothing too to avoid extending the range too quickly.
    The range for the level can be fixed now using press_range. #11

--- src/captouch_rp2.py
+++ src/captouch_rp2.py
@@ -3,8 +3,8 @@
 # SPDX-License-Identifier: MIT
 
 # This is https://github.com/AncientJames/jtouch/blob/main/jtouch.py
-# with a few tweaks and an async version
-
+# with a few tweaks and asyncio Pushbutton compatibility
+# See https://github.com/gurgleapps/Gurgle-Apps-Word-Clock/issues/11
 
 import rp2
 import machine
@@ -72,22 +72,37 @@ def capsense():
 
 
 class TouchPad:
+    """A PIO-based capacitive touch object with boolean and continuous values.
+
+    :param int pin: The GPIO pin number.
+    :param int sm_num: Optional PIO state machine number.
+    :param bool calibrate_now: Perform an immediate pad calibration. Touch pad must not be used during calibration. Default True.
+    :param int noise_threshold: The minimum noise threshold. Default 100.
+    :param int press_threshold: The count value which indicates a touch or None for 3% of untouched value. Default None.
+    :param int press_range: A fixed count range for the continuous scale or None for auto-ranging. Default None.
+    :param int warmup: the number of update calls to use to calibrate the touch pad before it's ready for use. Default 20.
+    """
+
     FIFO_LEN = 4 * 2  # RX FIFO is jopined with TX FIFO to enlarge it
     NOISE_MULTIPLIER = 4
     next_sm = 0
 
     def __init__(self, pin, sm_num=None, calibrate_now=True,
                  *,
-                 press_value=None):
-        self.warmup = 20
-        self.noise_threshold = 100
+                 noise_threshold=100,
+                 press_threshold=None,
+                 press_range=None,
+                 warmup=20):
+        self.warmup = warmup
+        self.noise_threshold = noise_threshold
         self.frequency = 125_000_000  # Does not have to divide into cpu clock
         self.cap_loop_count = _CAP_LOOP_MANTISSA * 2**_CAP_LOOP_EXPONENT
         self.est_s = self.cap_loop_count * 2 / self.frequency
         # Bare Pi Pico ratio is 22.9:1, Pi Pico 2 W with 6cm wire 26.5
         self.est_hi = round(self.cap_loop_count) // 24
         # 3% of 24000 count is 800
-        self.press_value = round(0.03 * self.est_hi) if press_value is None else press_value
+        self.press_threshold = round(0.03 * self.est_hi) if press_threshold is None else press_threshold
+        self.press_range = press_range
 
         self.level = 0
         self.last_value = 0
@@ -129,31 +144,48 @@ class TouchPad:
             # Discard any stale buffered values and
             # read a new one if FIFO was full
             for _ in range(fifo_cnt):
-                level = _U32MAX - self.state_machine.get()
+                raw_value = _U32MAX - self.state_machine.get()
             if fifo_cnt == 0 or fifo_cnt == self.FIFO_LEN:
-                level = _U32MAX - self.state_machine.get()
+                raw_value = _U32MAX - self.state_machine.get()
 
+            press_range = self.press_range
             if self.warmup > 0:
                 self.warmup -= 1
-                self.warmup_level_lo = min(level, self.warmup_level_lo)
-                self.warmup_level_hi = max(level, self.warmup_level_hi)
+                self.warmup_level_lo = min(raw_value, self.warmup_level_lo)
+                self.warmup_level_hi = max(raw_value, self.warmup_level_hi)
                 if self.warmup == 0:
                     self.noise_threshold = max(self.noise_threshold,
                                                self.NOISE_MULTIPLIER * (self.warmup_level_hi - self.warmup_level_lo))
+                    self.level_hi = self.warmup_level_hi
+                    if press_range is None:
+                        self.level_lo = min(self.warmup_level_lo, self.warmup_level_hi - self.press_threshold)
+                    else:
+                        self.level_lo = self.warmup_level_hi - press_range
             else:
-                self.level_lo = min(level, self.level_lo)
-                self.level_hi = max(level, self.level_hi)
+                decrease = self.level_hi - raw_value
+                level_range = self.level_hi - self.level_lo
+                if decrease > self.press_threshold:
+                    # If range tracking is enabled then slowly follow any outliers
+                    if press_range is None and raw_value < self.level_hi - level_range * 0.80:
+                        self.level_lo = (7 * self.level_lo + raw_value) / 8.0
+                else:
+                    # Very slowly follow any drift in untouched
+                    # or lightly touched values hoping the latter is rare
+                    self.level_hi = (511 * self.level_hi + raw_value) / 512.0
+                    if press_range is not None:
+                        self.level_lo = self.level_hi - press_range
 
-            level_range = self.level_hi - self.level_lo
-            self.last_value = level
-            if level_range > self.noise_threshold:
-                self.level = (self.level_hi - level) / level_range
+                self.level = min(1.0,
+                                 max(0.0,
+                                     decrease / level_range)) if decrease > self.noise_threshold else 0.0
+
+            self.last_value = raw_value
 
     def is_pressed(self):
         self.update()
-        return self.last_value < self.level_hi - self.press_value
+        return self.last_value < self.level_hi - self.press_threshold
 
     def __call__(self):
-        """This is intended for use with the unofficial asyncio primitive Pushbutton."""
+        """This is intended for use with the asyncio primitive Pushbutton."""
         self.update(True)
-        return self.last_value < self.level_hi - self.press_value
+        return self.last_value < self.level_hi - self.press_threshold
